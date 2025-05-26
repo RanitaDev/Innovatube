fd83cc93c46ec4eaa7a64c652cd4dea0191a4c73
Fernando Camarena Flores <fecaflo18@gmail.com>
2025-05-26 02:07:12 -0600

    Remove environment

--- frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api',
-  apiYoutubeUrl: 'https://www.googleapis.com/youtube/v3',
-  youtubeApiKey: 'AIzaSyAbfKDOVhP9A-v_zxvF-guO-knU5hKQayk'
-};
